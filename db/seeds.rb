7c17cc74aa0b89d9a6eaef530ecaaa943353edb2
lukas1056 <lukas.abreu1056@gmail.com>
2016-10-15 20:46:40 -0300

    Correções em seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -51,7 +51,7 @@ end
 ].each do |party|
   unless Party.where(abbreviation: party[:abbreviation]).exists?
     puts "==> Criando partido #{party[:abbreviation]}"
-    Party.create! party.merge({logo: no_photo})
+    Party.create! party
   end
 end
 
@@ -61,32 +61,35 @@ end
 ############## COUNCILLOR - BEGIN #################
 
 [
-  { name: 'Fernando Augusto Barp', avatar: '/uploads/councillors/3/councillor_c176e04ec4f7cd435186350f4ba3b233.jpg', party: 'PCdoB', voter_registration: '043824130973' },
-  { name: 'Claudemir de Araújo', avatar: '/uploads/councillors/9/councillor_9d1fe9e64dd79e527c496f278b0251c4.jpg', party: 'PTB', voter_registration: '057995110477' },
-  { name: 'Eni Maria Scandolara', avatar: '/uploads/councillors/10/councillor_4d8f1ea3a8070812d2461a2afae89076.jpg', party: 'PP', voter_registration: '15377220493' },
-  { name: 'Ernani Mario Coelho Mello', avatar: '/uploads/councillors/11/councillor_efd28b9ea313563129f8e7d7c1fb756c.jpg', party: 'PDT', voter_registration: '025790700469' },
-  { name: 'Jorge Valdair Psidonik', avatar: '/uploads/councillors/12/councillor_e9d873be7932ff9495a68945534331c1.jpg', party: 'PT', voter_registration: '059816150450' },
-  { name: 'José da Cruz', avatar: '/uploads/councillors/13/councillor_853aa3b3d1f56b2c794ab4d62a3bd33a.jpg', party: 'PMDB', voter_registration: '025741870400' },
-  { name: 'Leandro Augusto Basso', avatar: '/uploads/councillors/14/councillor_4ef64f65a6f12602848a434f1ba14965.jpg', party: 'PRB', voter_registration: '062722440418' },
-  { name: 'Lucas Roberto Farina', avatar: '/uploads/councillors/15/councillor_85738567d9de732a3f937c674e093eae.jpg', party: 'PT', voter_registration: '082414870442' },
-  { name: 'Luiz Deonísio Silva de Brito', avatar: '/uploads/councillors/16/councillor_9f5b8b4fb459a4c65c82b2afa638e737.jpg', party: 'PSD', voter_registration: '54468550485' },
-  { name: 'Marcos Antônio Lando', avatar: '/uploads/councillors/17/councillor_2f32dd0e6996d0862e9e182ffa1327e7.jpg', party: 'PDT', voter_registration: '019551170477' },
-  { name: 'Nadir Antônio Barbosa', avatar: '/uploads/councillors/18/councillor_1a66513c37574629ed3bb4c55da5f6ab.jpg', party: 'PMDB', voter_registration: '004197750450' },
-  { name: 'Alderi Antônio Oldra', avatar: '/uploads/councillors/19/councillor_2fb078f1ea6acc6cfb0072197ae90c2f.jpg', party: 'PT', voter_registration: '004604330442' },
-  { name: 'Sérgio Alves Bento', avatar: '/uploads/councillors/20/councillor_513445bfdcbdbbcf1c20f38af46ff8c0.jpg', party: 'PT', voter_registration: '031596040450' },
-  { name: 'Silvio Ambrózio', avatar: '/uploads/councillors/21/councillor_2c9865a0ebe29efbc3bb2a08021ff57f.jpg', party: 'PT', voter_registration: '51382720450' },
-  { name: 'Anacleto Zanella', avatar: '/uploads/councillors/22/councillor_a15fd219464cabd127b442ffbef131e1.jpg', party: 'PT', voter_registration: '32944040477' },
-  { name: 'Vânia Isabel Smaniotto Miola', avatar: '/uploads/councillors/23/councillor_517ddd0816d56b2d2fea113270fb8917.jpg', party: 'PMDB', voter_registration: '025760750400' },
-  { name: 'Edgar Paulo Marmentini', avatar: '/uploads/councillors/24/councillor_2dba627721ac3a33d45545322957d8e1.jpg', party: 'PMDB', voter_registration: '43242010442' }
+  { name: 'Fernando Augusto Barp', party: 'PCdoB' },
+  { name: 'Claudemir de Araújo', party: 'PTB' },
+  { name: 'Eni Maria Scandolara', party: 'PP' },
+  { name: 'Ernani Mario Coelho Mello', party: 'PDT' },
+  { name: 'Jorge Valdair Psidonik', party: 'PT' },
+  { name: 'José da Cruz', party: 'PMDB' },
+  { name: 'Leandro Augusto Basso', party: 'PRB' },
+  { name: 'Lucas Roberto Farina', party: 'PT' },
+  { name: 'Luiz Deonísio Silva de Brito', party: 'PSD' },
+  { name: 'Marcos Antônio Lando', party: 'PDT' },
+  { name: 'Nadir Antônio Barbosa', party: 'PMDB' },
+  { name: 'Alderi Antônio Oldra', party: 'PT' },
+  { name: 'Sérgio Alves Bento', party: 'PT' },
+  { name: 'Silvio Ambrózio', party: 'PT' },
+  { name: 'Anacleto Zanella', party: 'PT' },
+  { name: 'Vânia Isabel Smaniotto Miola', party: 'PMDB' },
+  { name: 'Edgar Paulo Marmentini', party: 'PMDB' }
 ].each do |councillor|
-  unless Councillor.where(voter_registration: councillor[:voter_registration]).exists?
+  unless Councillor.where(name: councillor[:name]).exists?
     puts "==> Criando vereador(a) #{councillor[:name]}"
 
     attributes = councillor.dup
+    names = councillor[:name].split(' ')
     attributes[:party] = Party.find_by!(abbreviation: attributes[:party])
-    attributes[:avatar] = open('http://www.camaraerechim.rs.gov.br' + attributes[:avatar]) rescue no_photo
 
-    Councillor.create! attributes
+    Councillor.create! attributes.merge({
+      username: names[0] + names[-1],
+      password: SecureRandom.base64  
+    })
   end
 end
 
