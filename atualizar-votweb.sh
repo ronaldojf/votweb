1fb491f73e9699787db4a1d2ec1f73a5d461d444
Ronaldo Fuzinato <ronaldofuzinato123@gmail.com>
2016-12-09 20:30:45 -0200

    Corrige configuração no banco de dados e adiciona script para atualizar automáticamente containers docker

--- atualizar-votweb.sh
+++ atualizar-votweb.sh
@@ -1,5 +1,13 @@
-# ATUALIZA VOTWEB
-
 cd $HOME/votweb
 docker pull votweb/votweb:latest
+
+WEB=$(docker-compose ps -q web)
+WORKDIR=$(docker exec $WEB pwd)
+
+docker cp $WEB:$WORKDIR/docker-compose.yml docker-compose.yml
+docker cp $WEB:$WORKDIR/atualizar-votweb.sh atualizar-votweb.sh
+chmod +x atualizar-votweb.sh
+
 docker-compose up -d
+
+echo "ATUALIZAÇÃO CONCLUÍDA!"
